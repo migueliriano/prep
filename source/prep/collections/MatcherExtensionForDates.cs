cd29e99eb96270afbbb9d166391b2e8178f4f14a
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2014-05-14 09:28:47 -0600

    Updated

---
+++ source/prep/collections/MatcherExtensionForDates.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using prep.utility.filtering;
+
+namespace prep.collections
+{
+  public static class MatcherExtensionForDates
+  {
+    public static IMatchAn<ItemToMatch> greater_than<ItemToMatch>(this MatcherCreationExtensionPoint<ItemToMatch, DateTime> extension_point, int year, DateValues date_value) 
+    {
+      if (date_value == DateValues.year)
+      {
+        throw new NotImplementedException("Finish");
+      }
+      throw new NotImplementedException("We don't support that yet");
+    }
+  }
+
+  public class DateValues
+  {
+    public static readonly DateValues year = new DateValues();
+  }
+}
